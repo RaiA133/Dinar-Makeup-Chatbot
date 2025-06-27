53e57e78ea653eb625dc922a69eb7b12c244c5ce
RaiA133 <rai010303@gmail.com>
2025-06-28 01:07:57 +0700

    update app.js

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use('/api/v1', routes)
 
-app.listen(process.env.PORT, () => {
-  console.log(`App listening to http://localhost:${process.env.PORT}`);
-})
+app.listen(port, '0.0.0.0', () => {
+  console.log(`🚀 Server ready at http://0.0.0.0:${port}`);
+});
+
 
 export default app;
