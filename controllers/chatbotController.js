701816ac3aece32bd7613a16fcdc46f0ca575766
RaiA133 <rai010303@gmail.com>
2025-07-03 01:15:10 +0700

    fix: chatbot prompt

--- controllers/chatbotController.js
+++ controllers/chatbotController.js
@@ -5,15 +5,15 @@ const chatbotController = {
   async healthCheck(req, res, next) {
     res.send('PONG')
   },
-
+  
   async chat(req, res, next) {
     const { message, history } = req.body;
     if (!message) return res.status(400).json({ error: 'Field "message" dibutuhkan' });
-
+  
     try {
       const system = `Nama anda dinar, anda adalah Assisten Virtual dari perusahaan Dinar Makeup, sebuah jasa Wedding Organizer dan juga Makeup Profesional.
-      jika user bertanya lokasi dari halaman atau cara yang biasanya ditanyakan untuk fungsi tour guide halaman, jangan beri info data, dan hanya boleh keterangan letak halaman
-      `
+      jika user bertanya lokasi dari halaman atau cara yang biasanya ditanyakan untuk fungsi tour guide halaman, jangan beri info data, 
+      hanya boleh keterangan letak halaman atau link dengan step step nya.`
       const data = await askChat(message, system, history);
       if (data) {
         res.status(200).json({
@@ -34,45 +34,76 @@ const chatbotController = {
   async guide(req, res, next) {
     const { message, history } = req.body;
     if (!message) return res.status(400).json({ error: 'Field "message" dibutuhkan' });
-
+  
     try {
       const system = `berikan saya output JSON berupa isi steps dari setOptions sesuai dengan pertanyaan
       yand dimana target element seperti tag, class, id diisi dari data di RAG HTML Halaman.
-      jika kemungkinan ada elemet target lain dengan identifier yang sama, buat isi taget element lebih 
+      jika ada elemet target lain dengan identifier yang sama, buat isi taget element lebih 
       panjang dengan menarget element jauh lebih panjang dari banyak data element parent nya.
-      CONTOH : 
-      [
+
+      CONTOH : [
         {
-          url: "<segment setelah base url misalkan : /pricing, /gallery, dll> (jangan ada url)>",
+          url: "<segment setelah base url misalkan : /pricing, /gallery, dll> (jangan ada url, hanya /<segment>",
           step: [
-          {
-            element: "document.querySelector('body > div > div > div:nth-child(1) > div:nth-child(1) > ul')",
-            intro: "Ini adalah navbar",
-            position: "bottom"
-          },
-          {
-            element: "document.querySelector('body > div > div > div:nth-child(1) > div:nth-child(2) > ul + a')",
-            intro: "Ini adalah tombol login,
-            position: "left"
-          },
-          {
-            element: "document.querySelector('body > div > div > div:nth-child(2) > div > div:nth-child(1) > section')",
-             intro: "Ini adalah list paket pernikahan",
-            position: "right"
-          },
-          ..... <bisa lebih banyak>
+            {
+              element: ".navbar",
+              intro: "Ini adalah navbar",
+              position: "bottom"
+            },
+            {              
+              element: ".hero-content",
+              intro: "Ini adalah Hero",
+              position: "right"
+            },
+            {
+              element: ".login",
+              intro: "Klik di sini untuk login",
+              position: "left"
+            },
+            ..... <dll>
           ]
         }
-        <setelah ini tidak boleh ada object lagi>
+        <setelah ini tidak boleh ada object lagi, hanya ada 1 url dan 1 step dengan banyak isi step>
       ]
-      `;
 
+      NOTE : 
+      - isi semua element nantinya akan dimasukan kedalam document.querySelector(),
+      jadi pastikan element isinya bisa dibacar oleh document.querySelector().
+      - isi semua element tidak boleh menggandung class dengan tanda ":" . misalkan ".sm:..." , ".md:..." , dll. jangan sertakan class ini
+      - url harus sesuaikan dengan semua data url yg ada, jangan beri segment yang tidak ada di semua link halaman webstite
+
+      HASIL JSON DIATAS AKAN DIPAKAI DISINI :
+      const resolveSteps = (stepsFromApi) => {
+        return stepsFromApi.map(step => {
+          return {
+            element: document.querySelector(step.element),
+            intro: step.intro,
+            position: step.position
+          };
+        });
+      };
+      const startTour = useCallback(() => {
+      setShowChatbot(false);
+      navigate(introJsSteps.url);
+      setTimeout(() => {
+          introJs()
+            .setOptions({
+              steps: resolveSteps(introJsSteps.step),
+              nextLabel: "next",
+              prevLabel: "back",
+              skipLabel: "x",
+              doneLabel: "done",
+              showProgress: true,
+            })
+            .start();
+        }, 1000);
+      }, [introJsSteps]);
+      `;
+  
       const data = await askChat(message, system, history);
-      // console.log(data);
+      console.log(data);
       if (data) {
         const cleaned = data.replace(/```json|```/g, '').trim();
-        console.log("=================================================");
-        // console.log(JSON.parse(cleaned));
         res.status(200).json({
           status: 200,
           message: "Success!",
@@ -87,8 +118,8 @@ const chatbotController = {
       });
     }
   }
-
-
+  
+  
 }
 
 export default chatbotController
