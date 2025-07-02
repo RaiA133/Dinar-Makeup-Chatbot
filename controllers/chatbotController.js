826d57571f5efa2bba3399b4260a408bb93411ee
RaiA133 <rai010303@gmail.com>
2025-07-02 21:13:59 +0700

    fix: promnt for chatGuide api#2

--- controllers/chatbotController.js
+++ controllers/chatbotController.js
@@ -35,36 +35,42 @@ const chatbotController = {
 
     try {
       const system = `berikan saya output JSON berupa isi steps dari setOptions sesuai dengan pertanyaan
-      yand dimana target element seperti tag, class, id diisi dari data di RAG HTML Halaman 
+      yand dimana target element seperti tag, class, id diisi dari data di RAG HTML Halaman.
+      jika kemungkinan ada elemet target lain dengan identifier yang sama, buat isi taget element lebih 
+      panjang dengan menarget element jauh lebih panjang dari banyak data element parent nya.
       contoh : 
       [
         {
-          url: "<link halaman>",
+          url: "<link halaman untuk kontent dibawah berupa segment setelah base url misalkan : /pricing>",
           step: [
           {
-            element: document.querySelector(".navbar"),
+            element: "document.querySelector('.navbar')",
             intro: "Ini adalah navbar,
             position: "bottom"
           },
           {
-            element: document.querySelector(".hero-content"),
+            element: "document.querySelector('.hero-content')",
              intro: "Ini adalah Hero",
             position: "right"
           },
           {
-            element: document.querySelector(".login"),
+            element: "document.querySelector('.login')",
             intro: "Klik di sini untuk login",
             position: "left"
           },
-          .....
+          ..... <bisa lebih banyak>
           ]
         }
+        <setelah ini tidak boleh ada object lagi>
       ]
       `;
 
       const data = await askChat(message, system, history);
+      // console.log(data);
       if (data) {
         const cleaned = data.replace(/```json|```/g, '').trim();
+        console.log("=================================================");
+        // console.log(JSON.parse(cleaned));
         res.status(200).json({
           status: 200,
           message: "Success!",
